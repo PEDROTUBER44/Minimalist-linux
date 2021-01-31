8541e224946f3b2b3925926fabfe7946ad9d7217
PEDROTUBER44 <77652411+PEDROTUBER44@users.noreply.github.com>
2021-01-31 15:01:29 -0300

    Add files via upload

---
+++ xfce-ubuntu_20_04.sh
@@ -0,0 +1,25 @@
+sudo apt update ;
+sudo apt upgrade -y ;
+
+sudo apt install xfce4-session xorg lightdm lightdm-gtk-greeter xfce4 policykit-1 xfce4-panel network-manager-gnome thunar xfce4-terminal pulseaudio xfce4-whiskermenu-plugin --no-install-recommends -y ;
+
+sudo rm -r /usr/share/themes/Bright ;
+sudo rm -r /usr/share/themes/Daloa ;
+sudo rm -r /usr/share/themes/Default-hdpi ;
+sudo rm -r /usr/share/themes/Default-xhdpi ;
+sudo rm -r /usr/share/themes/Raleigh ;
+sudo rm -r /usr/share/themes/Emacs ;
+sudo rm -r /usr/share/themes/Kokodi ;
+sudo rm -r /usr/share/themes/Retro ;
+sudo rm -r /usr/share/themes/Smoke ;
+sudo rm -r /usr/share/themes/ZOMG-PONIES\! ;
+sudo rm -r /usr/share/themes/Moheli ;
+
+sudo rm -r /usr/share/icons/hicolor ;
+sudo rm -r /usr/share/icons/Humanity ;
+sudo rm -r /usr/share/icons/Humanity-dark ;
+sudo rm -r /usr/share/icons/locolor ;
+sudo rm -r /usr/share/icons/ubuntu-mono-dark ;
+sudo rm -r /usr/share/icons/ubuntu-mono-light ;
+
+echo "xfce-ubuntu_20_04"
