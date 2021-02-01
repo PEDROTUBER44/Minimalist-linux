e50366051cb73649d1ab58d3a23531ddb78b091a
PEDROTUBER44 <77652411+PEDROTUBER44@users.noreply.github.com>
2021-02-01 20:50:12 -0300

    update 0.01

--- xfce.sh
+++ xfce.sh
@@ -1,7 +1,7 @@
 sudo apt update ;
 sudo apt upgrade -y ;
 
-sudo apt install xfce4-session xorg lightdm lightdm-gtk-greeter xfce4 policykit-1 xfce4-panel network-manager-gnome thunar xfce4-terminal pulseaudio xfce4-whiskermenu-plugin --no-install-recommends -y ;
+sudo apt install xorg lightdm lightdm-gtk-greeter xfce4-session xfce4 policykit-1 xfce4-panel network-manager network-manager-gnome nautilus xfce4-terminal pulseaudio xfce4-whiskermenu-plugin --no-install-recommends -y ;
 
 sudo rm -r /usr/share/themes/Bright ;
 sudo rm -r /usr/share/themes/Daloa ;
@@ -21,5 +21,9 @@ sudo rm -r /usr/share/icons/Humanity-dark ;
 sudo rm -r /usr/share/icons/locolor ;
 sudo rm -r /usr/share/icons/ubuntu-mono-dark ;
 sudo rm -r /usr/share/icons/ubuntu-mono-light ;
+
+sudo systemctl enable network-manager ;
+sudo systemctl enable lightdm ;
+sudo reboot ;
 
 echo "xfce-ubuntu_20_04"
