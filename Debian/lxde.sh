0d1fba972a522d5a434d345dc8c1d9384a2e9a97
PEDROTUBER44 <pedro.rosendops4@gmail.com>
2021-02-13 21:54:14 -0300

    update 0.04

--- Debian/lxde.sh
+++ Debian/lxde.sh
@@ -1,12 +1,22 @@
+sudo rm /var/lib/dpkg/lock-frontend ; 
+sudo rm /var/cache/apt/archives/lock ;
+
 sudo apt update ;
+
 sudo apt upgrade -y ;
 
-sudo apt install lxde-core -y ;
-sudo apt install network-manager pulseaudio xorg lightdm lxterminal --no-install-recommends -y ;
-sudo apt --purge remove xterm -y ;
+sudo apt install exfat-utils network-manager xorg lightdm pulseaudio lxde-core lxterminal lxrandr lxsession-default-apps lxappearance pcmanfm-desktop-pref xscreensaver-properties sudo system-config-printer policykit-1 libpolkit-qt5-1-dev --no-install-recommends -y ;
 
-sudo systemctl enable network-manager ;
-sudo systemctl enable lightdm ;
+sudo apt purge nano -y ;
+
+sudo apt autoremove -y ;
+
+sudo apt purge python2 -y ;
+ 
+sudo apt autoremove -y ;
+
+systemctl enable network-manager ;
+
+systemctl enable lightdm ;
+
 sudo reboot ;
-
-echo "lxde-debian"
